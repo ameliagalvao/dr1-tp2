201033bb1d5f54d8d3035ebbe0917e8021329250
amelia <ameliamgalvao@gmail.com>
2025-03-12 14:43:28 -0300

    Alterei o exercício três, pois não havia necessidade de usar o Jqwik uma vez que valores diversos não gerariam quaisquer mudanças no teste do método.

--- src/test/java/OrderServiceTest.java
+++ src/test/java/OrderServiceTest.java
@@ -1,41 +1,35 @@
-import net.jqwik.api.*;
 import org.example.OrderService;
 import org.example.PaymentProcessor;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class OrderServiceTest {
 
-    //Provedor Jqwik de valores doubles positivos aleatórios
-    @Provide
-    Arbitrary<Double> positiveDoubles() {
-        return Arbitraries.doubles().greaterThan(0.0);
-    }
-
-    //Testa um pagamento aprovado usando os valores aleatórios
-    @Property
-    public void testProcessOrderApprovedPayment(
-            @ForAll("positiveDoubles") double payment) {
+    //Testa pagamento aprovado (retorno True)
+    @Test
+    public void testProcessOrderApproved() {
         PaymentProcessor mockPaymentProcessor = Mockito.mock(PaymentProcessor.class);
-        when(mockPaymentProcessor.processPayment(payment)).thenReturn(true);
+        //Poderia ser qualquer valor, já que pelo que está escrito no código
+        //esse parâmetro não vai mudar nada no processamento da função.
+        double amount = 10.0;
+        when(mockPaymentProcessor.processPayment(amount)).thenReturn(true);
         OrderService orderService = new OrderService(mockPaymentProcessor);
-        boolean result = orderService.processOrder(payment);
+        boolean result = orderService.processOrder(amount);
         assertTrue(result);
-        verify(mockPaymentProcessor, times(1)).processPayment(eq(payment));
+        verify(mockPaymentProcessor, times(1)).processPayment(amount);
     }
 
-    @Property
-    void processOrderRejectedPayment(
-            @ForAll("positiveDoubles") double payment) {
-        PaymentProcessor mockPaymentProcessor = mock(PaymentProcessor.class);
-        when(mockPaymentProcessor.processPayment(anyDouble())).thenReturn(false);
+    //Testa pagamento é recusado (False)
+    @Test
+    public void testProcessOrderRejected() {
+        PaymentProcessor mockPaymentProcessor = Mockito.mock(PaymentProcessor.class);
+        double amount = 10.0;
+        when(mockPaymentProcessor.processPayment(amount)).thenReturn(false);
         OrderService orderService = new OrderService(mockPaymentProcessor);
-        boolean result = orderService.processOrder(payment);
+        boolean result = orderService.processOrder(amount);
         assertFalse(result);
-        verify(mockPaymentProcessor, times(1)).processPayment(eq(payment));
+        verify(mockPaymentProcessor, times(1)).processPayment(amount);
     }
 }
